bbe999c04dd18a10ff1699435f288304b8b0c3c2
Dromader2137 <wojciechowski.ignacy@gmail.com>
2023-11-10 19:01:22 +0100

    fsdfdf

--- config.fish
+++ config.fish
@@ -2,4 +2,5 @@ if status is-interactive
 	starship init fish | source
 	set fish_greeting   # Commands to run in interactive sessions can go here
 	alias ls="exa -l --group-directories-first"
+	contains $HOME/.cargo/bin/ $fish_user_paths; or set -Ua fish_user_paths $HOME/.cargo/bin/
 end
