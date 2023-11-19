ed0641150fdfc01f33477c55f34a86314092f61a
Dromader2137 <wojciechowski.ignacy@gmail.com>
2023-11-19 12:07:07 +0100

    sdfsdfsdfsdf

--- config.fish
+++ config.fish
@@ -1,6 +1,14 @@
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
+
 if status is-interactive
 	starship init fish | source
 	set fish_greeting   # Commands to run in interactive sessions can go here
-	alias ls="exa -l --group-directories-first"
+	alias ls="exa -l --group-directories-first -s Extension"
+	alias la="exa -la --group-directories-first -s Extension"
+	alias ll="exa -la --group-directories-first -s Extension -T -L 2"
 	contains $HOME/.cargo/bin/ $fish_user_paths; or set -Ua fish_user_paths $HOME/.cargo/bin/
 end
