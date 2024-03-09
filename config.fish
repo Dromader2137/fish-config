f58fde26bedf0257151ed9eff8a1297793f72cb4
Dromader2137 <wojciechowski.ignacy@gmail.com>
2024-03-09 12:41:52 +0100

    x

--- config.fish
+++ config.fish
@@ -1,21 +1,12 @@
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-		set WM $(gum choose "LeftWM" "Hyprland" "Qtile")
-		if [ $WM = "LeftWM" ]
-        	exec startx ~/.xinitrc leftwm -- -keeptty
-		else if [ $WM = "Qtile" ]
-			exec startx ~/.xinitrc qtile
-		else if [ $WM = "Hyprland" ]
-			Hyprland
-		end
-    end
-end
-
 if status is-interactive
-	starship init fish | source
+	# starship init fish | source
 	set fish_greeting   # Commands to run in interactive sessions can go here
+	export QT_QPA_PLATFORMTHEME=qt5ct
+	export LIBVA_DRIVER_NAME=nvidia
 	alias ls="exa -l --group-directories-first -s Extension"
 	alias la="exa -la --group-directories-first -s Extension"
 	alias ll="exa -la --group-directories-first -s Extension -T -L 2"
 	contains $HOME/.cargo/bin/ $fish_user_paths; or set -Ua fish_user_paths $HOME/.cargo/bin/
+	contains $HOME/renderdoc/build/bin/ $fish_user_paths; or set -Ua fish_user_paths $HOME/renderdoc/build/bin/
+	contains $HOME/.local/bin/ $fish_user_paths; or set -Ua fish_user_paths $HOME/.local/bin/
 end
